a8617a71f84bce156ffba9bef2c8c9e5c2051047
abnermateus <75183543+abnermateus@users.noreply.github.com>
2020-12-03 22:19:30 -0300

    Novas Alterações

--- passagem.js
+++ passagem.js
@@ -1,9 +1,10 @@
-let nomes = ['Pelé', 'Maradona', 'Cruyff', 'Messi', 'Ronaldo'];
-let name = document.querySelector('#nome');
+let nomes = ['Pelé', 'Maradona', 'Cruyff', 'Messi', 'Ronaldo','Buffon'];
+let name = document.querySelector('#name');
 let botao = document.querySelector('#botao');
-let lugar = 0;
+let posicao = 0;
 let imagens = ['Images/Pelé.jpg', 'Images/Maradona.jpg', 'Images/Cruyff.jpg', 'Images/Messi.jpg', 'Images/Ronaldo.jpg', 'Images/Buffon.jpg'];
-let desc = [] ;
+let desc = ['Considerado por muitos o melhor jogador de todos os tempos,<br>autor de mais de 1000 gols,criador de vários dribles<br>e 3 vezes campeão do Mundo,nosso rei Pelé.',
+'Maradona'] ;
 let foto = document.querySelector('#foto');
 let descri = document.querySelector('#descri');
 name.innerHTML = "<p>" + nomes[0] + "</p>";
@@ -11,11 +12,11 @@ foto.innerHTML = "<img src=" + imagens[0] + ">";
 descri.innerHTML = "<p>" + desc[0] + "</p>";
 
 botao.addEventListener('click',function(){
-  lugar ++;
-  if (lugar == 5){
-    lugar = 0;
+  posicao ++;
+  if(posicao == 6){
+    posicao = 0;
   }
-  name.innerHTML = "<p>" + nomes[lugar] + "</p>";
-  foto.innerHTML = "<img src=" + imagens[0] + ">";
-  descri.innerHTML = "<p>" + desc[lugar] + "</p>";
+  name.innerHTML = "<p>" + nomes[posicao] + "</p>";
+  foto.innerHTML = "<img src=" + imagens[posicao] + ">";
+  descri.innerHTML = "<p>" + desc[posicao] + "</p>";
 });
