8bb25aa811989a21ec503bc15951d05b544c1f63
abnermateus <75183543+abnermateus@users.noreply.github.com>
2020-12-06 16:10:50 -0300

    Alterações

--- champ.js
+++ champ.js
@@ -3,12 +3,12 @@ let name = document.querySelector('#name');
 let seta = document.querySelector('#seta');
 let posicao = 0;
 let imagens = ['Images/Copa.jpg', 'Images/Champions.jpg', 'Images/Euro.jpg', 'Images/Liberta.jpeg', 'Images/Mun.jpg', 'Images/Brasa.jpg'];
-let desc = ['O campeonato mais importante do Mundo.Todos os jogadores sonham em um dia<br> ganhar a Copa.Aqui as melhores seleções,<br>e os melhores jogadores batalham pela consagração eterna.',
-    'O campeonato mais importante que ocorre anualmente.Aqui as potências europeias jogam<br> entre elas para levantar a orelhuda.Ganhar a mesma é muito importante na carreira de um jogador.',
-    'Um campeonato inferior á Champions,é o segundo mais importante.Mas isso não significa<br>muita coisa,pois a magia que ocorre aqui é a mesma que acontece na champions.',
-    'Na minha opinião,o campeonato mais emocionante de todos.Os melhores times sul americanos<br>competem entre si por esse título.',
-    'Um campeonato em que os vencedores de suas copas continentais se enfrentam.O vencedor é<br>coroado como o melhor time do mundo.',
-    'O nosso campeonato nacional e o maior evento dos domingos.Aqui nossos times do coração se<br>enfrentam.Grandes jogadores foram revelados nesse campeonato,como Pelé,Neymar e Ronaldinho Gaucho.'
+let desc = ['O campeonato mais importante do Mundo. Todos os jogadores sonham em um dia<br> ganhar a Copa. Aqui as melhores seleções<br>e os melhores jogadores batalham pela consagração eterna.',
+    'O campeonato mais importante que ocorre anualmente. Aqui as potências europeias disputam<br>para levantar a orelhuda. Ganhar a mesma é muito importante na carreira de um jogador.',
+    'Um campeonato inferior à Champions, é o segundo mais importante da Europa. Mas isso não significa<br>muita coisa, pois a magia que ocorre aqui é a mesma que acontece na Champions.',
+    'Na minha opinião, o campeonato mais emocionante de todos. Os melhores times sul americanos<br>competem entre si por esse título.',
+    'Um campeonato em que os vencedores de suas copas continentais se enfrentam. O vencedor é<br>coroado como o melhor time do mundo.',
+    'O nosso campeonato nacional e o maior evento dos domingos. Aqui nossos times do coração se<br>enfrentam. Grandes jogadores foram revelados nesse campeonato, como Pelé, Neymar e Ronaldinho Gaucho.'
 ];
 let foto = document.querySelector('#foto');
 let descri = document.querySelector('#descri');
--- criadores.js
+++ criadores.js
@@ -3,11 +3,12 @@ let name = document.querySelector('#name');
 let seta = document.querySelector('#seta');
 let posicao = 0;
 let imagens = ['Images/endo.jpeg', 'Images/fubuki.jpeg', 'Images/ichinose.jpeg', 'Images/someoka.jpeg'];
-let foto = document.querySelector('#foto');
-let cria = document.querySelector('.cria');
-let descri = document.querySelector('#descri');
+let desc = ['O líder do grupo, é um aluno aplicado e bastante extrovertido.', 'Rei dos speedruns, esperançoso e feliz.', 'Esse aí é o mestre dos magos em pessoa.', 'Ninguém conhece, ninguém nunca viu.'];
+let picture = document.querySelector('#picture');
+let agente = document.querySelector('#agente');
 name.innerHTML = "<p>" + nomes[0] + "</p>";
-foto.innerHTML = "<img src=" + imagens[0] + ">";
+picture.innerHTML = "<img src=" + imagens[0] + ">";
+agente.innerHTML = "<p>" + desc[0] + "</p>";
 
 seta.addEventListener('click', function() {
     posicao++;
@@ -15,5 +16,6 @@ seta.addEventListener('click', function() {
         posicao = 0;
     }
     name.innerHTML = "<p>" + nomes[posicao] + "</p>";
-    foto.innerHTML = "<img src=" + imagens[posicao] + ">";
+    picture.innerHTML = "<img src=" + imagens[posicao] + ">";
+    agente.innerHTML = "<p>" + desc[posicao] + "</p>";
 });
