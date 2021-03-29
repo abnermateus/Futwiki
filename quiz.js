20e8c4ec9872591c94a8f7ee37ba57be746487f2
abnermateus <75183543+abnermateus@users.noreply.github.com>
2021-03-29 11:49:51 -0300

    Commit

--- quiz.js
+++ quiz.js
@@ -1,157 +1,159 @@
-const start = document.getElementById("start");
+const comecar = document.getElementById("comecar");
 const quiz = document.getElementById("quiz");
-const question = document.getElementById("question");
-const choiceA = document.getElementById("A");
-const choiceB = document.getElementById("B");
-const choiceC = document.getElementById("C");
-const choiceD = document.getElementById("D");
-const choiceE = document.getElementById("E");
-const counter = document.getElementById("counter");
+const questao = document.getElementById("questao");
+const opcaoA = document.getElementById("A");
+const opcaoB = document.getElementById("B");
+const opcaoC = document.getElementById("C");
+const opcaoD = document.getElementById("D");
+const opcaoE = document.getElementById("E");
+const contador = document.getElementById("contador");
 const timeGauge = document.getElementById("timeGauge");
-const progress = document.getElementById("progress");
-const scoreDiv = document.getElementById("scoreContainer");
+const progresso = document.getElementById("progresso");
+const scoreDiv = document.getElementById("pontuacao");
 
-let questions = [{
-        question: "Qual foi o time que mais ganhou títulos da Copa do Mundo?",
-        choiceA: "Alemanha",
-        choiceB: "Espanha",
-        choiceC: "Inglaterra",
-        choiceD: "Brasil",
-        choiceE: "Argentina",
+let questoes = [{
+        questao: "Qual foi o time que mais ganhou títulos da copa do mundo?",
+        opcaoA: "Alemanha",
+        opcaoB: "Espanha",
+        opcaoC: "Inglaterra",
+        opcaoD: "Brasil",
+        opcaoE: "Argentina",
         correct: "D"
     },
 
     {
-        question: "Qual jogador possui mais Bolas de Ouro?",
-        choiceA: "Pelé",
-        choiceB: "Messi",
-        choiceC: "Neymar",
-        choiceD: "Ronaldo Fenômeno",
-        choiceE: "Cristiano Ronaldo",
+        questao: "Qual jogador possui mais bolas de ouro?",
+        opcaoA: "Pelé",
+        opcaoB: "Messi",
+        opcaoC: "Neymar",
+        opcaoD: "Ronaldo Fenômeno",
+        opcaoE: "Cristiano Ronaldo",
         correct: "B"
     },
 
     {
-        question: "Qual time tem mais títulos da libertadores?",
-        choiceA: "Santos",
-        choiceB: "Boca Juniors",
-        choiceC: "Cruzeiro",
-        choiceD: "Independiente",
-        choiceE: "River Plate",
+        questao: "Qual time tem mais títulos da libertadores?",
+        opcaoA: "Santos",
+        opcaoB: "Boca Juniors",
+        opcaoC: "Cruzeiro",
+        opcaoD: "Independiente",
+        opcaoE: "River Plate",
         correct: "D"
+
     },
 
     {
-        question: "Qual dos seguintes times tem mais títulos de Champions League?",
-        choiceA: "Real Madrid",
-        choiceB: "Barcelona",
-        choiceC: "Juventus",
-        choiceD: "Bayern de Munique",
-        choiceE: "Botafogo",
+        questao: "Que time tem mais títulos de Champions League?",
+        opcaoA: "Real Madrid",
+        opcaoB: "Barcelona",
+        opcaoC: "Juventus",
+        opcaoD: "Bayern de Munique",
+        opcaoE: "Botafogo",
         correct: "A"
     },
 
     {
-        question: "Qual seleção ganhou a Copa do Mundo de 2018?",
-        choiceA: "Holanda",
-        choiceB: "Alemanha",
-        choiceC: "Bélgica",
-        choiceD: "Croácia",
-        choiceE: "França",
+        questao: "Qual time que ganhou a copa do mundo de 2018?",
+        opcaoA: "Holanda",
+        opcaoB: "Alemanha",
+        opcaoC: "Bélgica",
+        opcaoD: "Croácia",
+        opcaoE: "França",
         correct: "E"
     }
 ]
 
-const lastQuestion = questions.length - 1;
-let runningQuestion = 0;
+const ultimaQuestao = questoes.length - 1;
+let questaoAtual = 0;
 let count = 0;
-const questionTime = 10;
+const tempoDaQuestao = 15;
 const gaugeWidth = 150;
-const gaugeUnit = gaugeWidth / questionTime;
+const gaugeUnit = gaugeWidth / tempoDaQuestao;
 let TIMER;
-let score = 0;
+let pontos = 0;
 
-function renderQuestion() {
-    let q = questions[runningQuestion];
-    question.innerHTML = "<p>" + q.question + "</p>";
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
-    choiceD.innerHTML = q.choiceD;
-    choiceE.innerHTML = q.choiceE;
+function Questao() {
+    let q = questoes[questaoAtual];
+
+    questao.innerHTML = "<p>" + q.questao + "</p>";
+    opcaoA.innerHTML = q.opcaoA;
+    opcaoB.innerHTML = q.opcaoB;
+    opcaoC.innerHTML = q.opcaoC;
+    opcaoD.innerHTML = q.opcaoD;
+    opcaoE.innerHTML = q.opcaoE;
 }
 
-start.addEventListener("click", startQuiz);
+comecar.addEventListener("click", comecarQuiz);
 
-function startQuiz() {
-    start.style.display = "none";
-    renderQuestion();
+function comecarQuiz() {
+    comecar.style.display = "none";
+    Questao();
     quiz.style.display = "block";
-    renderProgress();
-    renderCounter();
-    TIMER = setInterval(renderCounter, 1000);
+    Progresso();
+    Contador();
+    TIMER = setInterval(Contador, 1000);
 }
 
-function renderProgress() {
-    for (let qIndex = 0; qIndex <= lastQuestion; qIndex++) {
-        progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
+function Progresso() {
+    for (let qIndex = 0; qIndex <= ultimaQuestao; qIndex++) {
+        progresso.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
     }
 }
 
-function renderCounter() {
-    if (count <= questionTime) {
-        counter.innerHTML = count;
+function Contador() {
+    if (count <= tempoDaQuestao) {
+        contador.innerHTML = count;
         timeGauge.style.width = count * gaugeUnit + "px";
         count++
     } else {
         count = 0;
-        answerIsWrong();
-        if (runningQuestion < lastQuestion) {
-            runningQuestion++;
-            renderQuestion();
+        respostaErrada();
+
+        if (questaoAtual < ultimaQuestao) {
+            questaoAtual++;
+            Questao();
         } else {
             clearInterval(TIMER);
-            scoreRender();
+            Pontuacao();
         }
     }
 }
 
-function checkAnswer(answer) {
-    if (answer == questions[runningQuestion].correct) {
-        score++;
-        answerIsCorrect();
+function verificarResposta(resposta) {
+    if (resposta == questoes[questaoAtual].correct) {
+        pontos++;
+        respostaCorreta();
     } else {
-
-        answerIsWrong();
+        respostaErrada();
     }
     count = 0;
-    if (runningQuestion < lastQuestion) {
-        runningQuestion++;
-        renderQuestion();
+    if (questaoAtual < ultimaQuestao) {
+        questaoAtual++;
+        Questao();
     } else {
         clearInterval(TIMER);
-        scoreRender();
+        Pontuacao();
     }
 }
 
-function answerIsCorrect() {
-    document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
+function respostaCorreta() {
+    document.getElementById(questaoAtual).style.backgroundColor = "#0f0";
 }
 
-function answerIsWrong() {
-    document.getElementById(runningQuestion).style.backgroundColor = "#f00";
+function respostaErrada() {
+    document.getElementById(questaoAtual).style.backgroundColor = "#f00";
 }
 
-function scoreRender() {
+function Pontuacao() {
     scoreDiv.style.display = "block";
-    const scorePerCent = Math.round(100 * score / questions.length);
 
-    let img = (scorePerCent >= 80) ? "img/5.png" :
-        (scorePerCent >= 60) ? "img/4.png" :
-        (scorePerCent >= 40) ? "img/3.png" :
-        (scorePerCent >= 20) ? "img/2.png" :
+    const porcentagem = Math.round(100 * pontos / questoes.length);
+
+    let img = (porcentagem >= 80) ? "img/5.png" :
+        (porcentagem >= 60) ? "img/4.png" :
+        (porcentagem >= 40) ? "img/3.png" :
+        (porcentagem >= 20) ? "img/2.png" :
         "img/1.png";
 
-    scoreDiv.innerHTML += "O Resultado  é";
-    scoreDiv.innerHTML += "<p>" + scorePerCent + "%</p>";
+    scoreDiv.innerHTML += "<p>" + porcentagem + "%</p>";
 }
